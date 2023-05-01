43df9cac6cb76990c5f84ecd0c97ce1f8e460d9c
Mecahnical noob <you@example.com>
2023-05-02 00:42:44 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Guid } from 'guid-typescript';
+import { Todo } from 'src/models/todo.model';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoapp';
+  title = 'To-do List';
+  todos:Todo[]=[
+    //new Todo(Guid.create(), 'this is a intial test',false)
+  ]
+  onSubmit(form:NgForm){
+    let todo = new Todo(Guid.create(),form.value.title,false)
+    this.todos.push(todo)
+    form.resetForm()
+  }
+  onComplete(id:Guid){
+    let todo = this.todos.filter(x=>x.id === id)[0]
+    todo.isComplete = !todo.isComplete
+  }
+  onDelete(id:Guid){
+    let todo = this.todos.filter(x=>x.id === id)[0]
+    let index = this.todos.indexOf(todo,0)
+    if(index>-1){
+      this.todos.splice(index,1)
+    }
+  }
 }
